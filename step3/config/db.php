038509c73232ad70d1073ec566daa75586adac05
Denys Sherstiuk <60985042+DenyaOver@users.noreply.github.com>
2022-11-15 10:11:20 +0100

    update
    composer update

--- step3/config/db.php
+++ step3/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tets',
+    'dsn' => 'mysql:host=localhost;dbname=pets',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
